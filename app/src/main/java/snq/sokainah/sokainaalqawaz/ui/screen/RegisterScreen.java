7f6850c9294e33216d38543c60ac04fe65225544
Sokainahalqawaz <sokaina155@hotmail.com>
2021-10-18 10:36:54 +0300

    Initial commit

---
+++ app/src/main/java/snq/sokainah/sokainaalqawaz/ui/screen/RegisterScreen.java
@@ -0,0 +1,16 @@
+package snq.sokainah.sokainaalqawaz.ui.screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import snq.sokainah.sokainaalqawaz.R;
+
+public class RegisterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+    }
+}
